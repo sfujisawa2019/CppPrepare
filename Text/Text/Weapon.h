f7e159b2473e2bf7c99ee1f4cf2ebcd6f8ddb737
fujisawa <sfujisawa2019@gmail.com>
2019-11-27 14:21:25 +0900

    基本状態

--- Text/Text/Weapon.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-// •ŠíƒNƒ‰ƒX
-class Weapon
-{
-public:
-	std::string name;
-};
--- Text/Text/main.cpp
+++ Text/Text/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-#include "Sumo.h"
-#include "Karateka.h"
 using namespace std;
 
 int main() {
-
-Sumo* sumo1 = new Sumo("”’–Q");
-Karateka* karateka1 = new Karateka("‘åR");
-
-// sumo1‚ªkarateka1‚ÉŸ—˜
-sumo1->WinKarateka(karateka1);
-	
-delete sumo1;
-delete karateka1;
 
 	system("pause");
 	return 0;
